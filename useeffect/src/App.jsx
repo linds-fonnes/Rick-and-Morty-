5c6ad4308d09e71afb63a99fa5075e6f7b9ce26e
Lindsey <linds.fonnes@gmail.com>
2020-12-09 20:39:06 -0700

    rendered pictures and names

--- useeffect/src/App.jsx
+++ useeffect/src/App.jsx
@@ -1,9 +1,29 @@
 import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+import CharacterList from "./components/CharacterList"
+
 
 function App() {
+  const [characters,setCharacters] = useState([]);
+
+  
+useEffect(() => {
+  axios.get('https://rickandmortyapi.com/api/character')
+  .then(res => {
+    setCharacters(res.data.results)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  },[])
+
   return (
     <div>
-    <h1>Hello</h1>
+
+      <CharacterList characters={characters}/>
+   
     </div>
   );
 }
---
+++ useeffect/src/components/CharacterList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CharacterCard from './CharacterCard'
+export default function CharacterList(props) {
+    const {characters} = props;
+    return (
+        <div>
+            {characters.map(character=> {
+                return <CharacterCard key={character.id} character={character}/>
+            })}
+        </div>
+    )
+}
